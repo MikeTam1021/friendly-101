0d57844753f931f93b19d893855b5f009f74d88f
Richard Cornish <richardcornish@gmail.com>
2015-03-18 10:43:17 -0500

    Added example Django project

---
+++ friendly101/friendly101/settings.py
@@ -0,0 +1,112 @@
+"""
+Django settings for friendly101 project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+import os
+import dj_database_url
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Website settings
+
+SECRET_KEY = os.environ.get('FRIENDLY101_SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG', True)
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+]
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'friendly101.urls'
+
+WSGI_APPLICATION = 'friendly101.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+
+################################
+### HEROKU-SPECIFIC SETTINGS ###
+################################
+
+# https://devcenter.heroku.com/articles/getting-started-with-django
+
+ALLOWED_HOSTS += [
+    '.herokuapp.com'
+]
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(
+    default='sqlite:///{}'.format(DATABASES['default']['NAME'])
+)
+
+# Enable Connection Pooling for PostgreSQL (if desired)
+if DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql_psycopg2':
+    DATABASES['default']['ENGINE'] = 'django_postgrespool'
