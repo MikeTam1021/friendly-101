57537b1bd83de7582a9c065cc0433a54880f8808
Richard Cornish <richardcornish@gmail.com>
2015-03-18 13:47:12 -0500

    Using whitenoise in wsgi

--- friendly101/friendly101/wsgi.py
+++ friendly101/friendly101/wsgi.py
@@ -20,6 +20,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friendly101.settings")
 ################################
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
