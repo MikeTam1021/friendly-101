fc997a9174dbf445f86e37523b9a95b79c8ba3c9
Richard Cornish <richardcornish@gmail.com>
2015-03-18 19:55:17 -0500

    Fixed the comment in the URLconf

--- friendly101/friendly101/urls.py
+++ friendly101/friendly101/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns(
-    """
-    Django should display the "It worked!" page without editing the project
-    URLconf, which originally shows the admin and comments out the homepage:
+"""
+Django should display the "It worked!" page without editing the original
+URLconf, which lists the commented-out homepage and admin:
 
-    # url(r'^$', 'friendly101.views.home', name='home'),
+# url(r'^$', 'friendly101.views.home', name='home'),
 
-    However, Heroku seems to change this default behavior and returns a
-    404 not found for the homepage. Therefore, I explicitly set the
-    the URL for home to the "It worked!" `default_urlconf` view.
-    """
-    '',
+However, Heroku seems to change this default behavior and returns a
+404 not found for the homepage. Therefore, I explicitly set the
+the URL for home to the "It worked!" `default_urlconf` view.
+"""
+
+urlpatterns = patterns('',
 
     url(r'^$', 'django.views.debug.default_urlconf', name='home'),
 
