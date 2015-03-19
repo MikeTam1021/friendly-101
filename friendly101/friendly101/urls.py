242b5e3cd956e4f4cc7612ccfed50312b2cb36e1
Richard Cornish <richardcornish@gmail.com>
2015-03-18 19:49:06 -0500

    Explicitly set the homepage to the "It worked!" view

--- friendly101/friendly101/urls.py
+++ friendly101/friendly101/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = patterns(
+    """
+    Django should display the "It worked!" page without editing the project
+    URLconf, which originally shows the admin and comments out the homepage:
+
     # url(r'^$', 'friendly101.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    However, Heroku seems to change this default behavior and returns a
+    404 not found for the homepage. Therefore, I explicitly set the
+    the URL for home to the "It worked!" `default_urlconf` view.
+    """
+    '',
+
+    url(r'^$', 'django.views.debug.default_urlconf', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
+
 )
